4c6ca8a442f82d56f42619aad63b87097ad11cd3
lipengbo <1162947801@qq.com>
2022-11-21 11:42:05 +0800

    ASM注册Demo初步完成

---
+++ app_lib_interface/src/main/java/com/billy/app_lib_interface/CategoryC.java
@@ -0,0 +1,15 @@
+package com.billy.app_lib_interface;
+
+/**
+ * 描述
+ *
+ * @author Created by lipengbo
+ * @email 1162947801@qq.com
+ * @time Created on 2022/11/19 15:05
+ */
+public class CategoryC implements ICategory{
+    @Override
+    public String getName() {
+        return "CategoryC";
+    }
+}
---
+++ app_lib_interface/src/main/java/com/billy/app_lib_interface/CategoryD.java
@@ -0,0 +1,15 @@
+package com.billy.app_lib_interface;
+
+/**
+ * 描述
+ *
+ * @author Created by lipengbo
+ * @email 1162947801@qq.com
+ * @time Created on 2022/11/21 10:36
+ */
+public class CategoryD implements ICategory{
+    @Override
+    public String getName() {
+        return "CategoryD";
+    }
+}
